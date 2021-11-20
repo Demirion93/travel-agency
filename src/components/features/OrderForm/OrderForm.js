af0b25090eecbe002841046804a9fb5ae3c75123
Szymon Wierucki <szymon.wierucki@gmail.com>
2021-11-20 01:50:08 +0100

    Add code for task 18.3

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import styles from './OrderForm.scss';
-import {Grid, Row, Col} from 'react-flexbox-grid';
-import OrderSummary from '../../features/OrderSummary/OrderSummary';
-import PageTitle from '../../common/PageTitle/PageTitle';
+import {Row, Col} from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+// import PageTitle from '../../common/PageTitle/PageTitle';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = ({tripCost, options}) => (
-<Grid>
+const OrderForm = ({tripCost, options, setOrderOption}) => (
   <Row>
+    {pricing.map((option) => 
+      <Col md={4} key={option.id}>
+        <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption}/>
+      </Col>
+    )}
     <Col xs={12}>
-      <PageTitle text='Trip options' />
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
   </Row>
-</Grid>
 );
 
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
+  setOrderOption: PropTypes.func,
+  
 };
 
 export default OrderForm;
